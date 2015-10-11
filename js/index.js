4a6743da022d5372c299b2bd6d1bfe902c9230a8
Travis Erwin <traviserwin3@gmail.com>
2015-10-10 23:39:01 -0500

    Added more names.

--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
 var populateName = function(firstName, lastName) {
 
   var firstList = [
-    'Adams','Afton','Alamo','Ardmore','Ready','Ridge','Roan','Taft','Talbott','Turtle','Trezevant','Tiny','Whitwell','Sardis','Shiloh','Rutledge','Rives','Pleasant View','Parsons','Pelham','McDonald','Mansfield','Macon','Jasper','old','Mohawk',
+  'Autumn','Flat','Adams','Afton','Alamo','Ardmore','Ready','Ridge','Roan','Taft','Talbott','Turtle','Trezevant','Tiny','Whitwell','Sardis','Shiloh','Rutledge','Rives','Pleasant View','Parsons','Pelham','McDonald','Mansfield','Macon',
+  'Jasper','old','Mohawk','Bright','Birch','Black','Cedar','Cotton','Cherry','Elm','Flat','Gold','Golden','Maple','Marble','New','Oak','Ocean','Old','Orange','Pine','Rich','River','Rock','Rose','Spring',
+  'Silver','Spruce','Stone','Sun','Sweet','White'
   ];
 
 
   var secondList = [
-   'ville','','Creek','burg','Home','Port','land','field','fort','top'
+   'Port','Mont','View','Bank','Bay','Ville','Island','Falls','Field','Way','Land','Water','Hills','Hill','Wood','Woods','Town','Ridge','Hurst','Mills','Heights','Wells','Shore','Shores','Spring','Springs',
+   'Bluff', 'Terrace','Ford','Grove','Valley','Peak','Park','Parke','Lake','Holler','Square','Pass','Junction','District','Brook'
   ];
 
   var randomFirstName = firstList[Math.floor(Math.random() * firstList.length)];
